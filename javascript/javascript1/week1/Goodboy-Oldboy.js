a1698b9e8e6dd0730eb72f1993bea4fbd8e2b6f7
suvarna <suvarna.akkala@gmail.com>
2021-06-27 22:27:23 +0200

    Task-2

---
+++ javascript/javascript1/week1/Goodboy-Oldboy.js
@@ -0,0 +1,12 @@
+let dogYearOfBirth = 2017
+let dogYearFuture = 2027
+let dogYear = dogYearFuture - dogYearOfBirth
+//console.log(dogYear);
+let shouldShowResultInDogYears = true
+if(shouldShowResultInDogYears)
+{
+    console.log('your dog will be ${dogYear*7} dog years old in ${dogYearFuture}')
+}
+else{
+    console.log('your dog will be ${dogYear} human years old in ${dogYearFuture}')
+}
