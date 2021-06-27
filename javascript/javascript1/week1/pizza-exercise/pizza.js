60e3bc3ab9af8931712337f62e704cd9df59c788
suvarna <suvarna.akkala@gmail.com>
2021-06-27 21:15:28 +0200

    initial commit

---
+++ javascript/javascript1/week1/pizza-exercise/pizza.js
@@ -0,0 +1,13 @@
+const favPizza = "Dominos";
+const pizzaPrice = 80;
+
+let amountOfPizza = 2;
+let familySize = false;
+let totalPrice = pizzaPrice * amountOfPizza;
+
+if (familySize) {
+    totalPrice *= 2;
+    console.log(`New pizza order: ${amountOfPizza} family ${favPizza}. The price of the pizza is: ${totalPrice}`);
+} else {
+    console.log(`New pizza order: ${amountOfPizza} ${favPizza}. The price of the pizza is: ${totalPrice}`);
+}
