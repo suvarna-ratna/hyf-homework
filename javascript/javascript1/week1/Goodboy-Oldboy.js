70435200b8246d689cd627dd3b56927b604322d6
suvarna <suvarna.akkala@gmail.com>
2021-06-28 08:54:43 +0200

    little change

--- javascript/javascript1/week1/Goodboy-Oldboy.js
+++ javascript/javascript1/week1/Goodboy-Oldboy.js
@@ -1,6 +1,6 @@
-let dogYearOfBirth = 2017
-let dogYearFuture = 2027
-let dogYear = dogYearFuture - dogYearOfBirth
+let dogYearOfBirth = 2017;
+let dogYearFuture = 2027;
+let dogYear = dogYearFuture - dogYearOfBirth;
 //console.log(dogYear);
 let shouldShowResultInDogYears = true
 if(shouldShowResultInDogYears)
