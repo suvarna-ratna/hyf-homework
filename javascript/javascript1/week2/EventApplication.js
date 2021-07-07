7b80762a14cea8fdeefe3e7d5d288abcb8148e7f
suvarna <suvarna.akkala@gmail.com>
2021-07-07 20:18:52 +0200

    Task2

---
+++ javascript/javascript1/week2/EventApplication.js
@@ -0,0 +1,9 @@
+const todayDate = new Date()
+const day = todayDate.getDay()
+const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+function getEventWeekday(eventInDays) {
+    const eventDay = (eventInDays + day)%7
+    return eventDay
+}
+console.log(getEventWeekday(9)) 
+console.log(getEventWeekday(2)) 
