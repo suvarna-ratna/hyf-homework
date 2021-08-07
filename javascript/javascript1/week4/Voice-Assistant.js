8aa948238e8802d295b85c11ec3de197793315e7
suvarna <suvarna.akkala@gmail.com>
2021-08-07 12:28:05 +0200

    feedback Updated

--- javascript/javascript1/week4/Voice-Assistant.js
+++ javascript/javascript1/week4/Voice-Assistant.js
@@ -1,41 +1,28 @@
-let vname = '';
+let voiceAssistantName = '';
 const toDo = [];
 function getReply(command) {
   const arrayOfStrings = command.split(" ");
   
   if (command.startsWith("Hello my name is")) {
-    vname = arrayOfStrings[4];
-    return `Nice to meet you ${vname}.` ;
+    voiceAssistantName = arrayOfStrings[4];
+    return `Nice to meet you ${voiceAssistantName}.` ;
   }
-  if (command =="What is my name?") {
-    return `Your name is ${vname}.`;
+  if (command ==="What is my name?") {
+    return `Your name is ${voiceAssistantName}.`;
   }
-  if (command == "Add fishing to my todo") {
-    toDo.push(command);
-    return "fishing added to my todo";
+  if (command.startsWith("Add")) {
+    toDo.push(arrayOfStrings.slice(1,arrayOfStrings.length-3).join(' '));
+    return `${arrayOfStrings.slice(1,arrayOfStrings.length-3).join( )} added to my todo`;
   }
-  if (command =="Add singing in the shower to my todo") {
-    toDo.push(command);
-    return "singing in the shower";
+  if (command.startsWith("remove")) {
+    let index =toDo.indexOf(arrayOfStrings.slice(1,arrayOfStrings.length-3).join(' '));
+    toDo.splice(index,1)
+    return `${arrayOfStrings.slice(1,arrayOfStrings.length-3).join( )} removed from my todo`;
   }
-  if (command =="Add dancing to my todo") {
-    toDo.push(command);
-    console.log(toDo)
-    return "dancing added to your todo";
+  if (command ==="What is on my todo?") {
+    return toDo.length>0 ? toDo : 'no Work'
   }
-  if (command =="remove fishing from my todo") {
-    toDo.shift();
-    return "Removed fishing from my todo";
-  }
-  if (command =="What is on my todo?") {
-    if(toDo.length>0){
-        return toDo;
-    } 
-    else {
-        return "no work";
-    }
-  }
-  if (command =="What day is it today?") {
+  if (command ==="What day is it today?") {
     let dateArr = new Date().toDateString().split(" ");
     return `${dateArr[2]}. of ${dateArr[1]} ${dateArr[3]}`;
   }
@@ -75,6 +62,7 @@ console.log(getReply("What is my name?")); // "Your name is Benjamin"
 console.log(getReply("Add fishing to my todo")); // "fishing added to your todo"
 console.log(getReply("Add singing in the shower to my todo")); // "singing in the shower"
 console.log(getReply("Add dancing to my todo")); // "dancing added to your todo"
+console.log(getReply("Add cooking to my todo")); // "dancing added to your todo"
 console.log(getReply("remove fishing from my todo")); // "Removed fishing from my todo"
 console.log(getReply("What is on my todo?")); // "Respond with current todo"
 console.log(getReply("What day is it today?")); // "Respond with current date like 02. of Aug 2021"
