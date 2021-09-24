c90e418e148c3a4c38b50f28f240fea469081252
suvarna <suvarna.akkala@gmail.com>
2021-09-24 16:10:20 +0200

    feedback implemented

--- javascript/javascript1/week1/Age-ify.js
+++ javascript/javascript1/week1/Age-ify.js
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
-const yearOfBirth = 1992
-const yearFuture = 2027
-const age = yearFuture-yearOfBirth
-=======
-let yearOfBirth = 1992
-let yearFuture = 2027
-let age = yearFuture-yearOfBirth
->>>>>>> master
+const yearOfBirth = 1992;
+const yearFuture = 2027;
+const age = yearFuture - yearOfBirth;
 
-console.log(`You will be ${age} ears old in ${yearFuture}`)
+console.log(`You will be ${age} ears old in ${yearFuture}`);
