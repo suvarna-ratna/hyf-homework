60ab88961efd5f2113906d62042fdb69bf93ef58
suvarna <suvarna.akkala@gmail.com>
2021-09-24 10:04:57 +0200

    feedback implemented

--- javascript/javascript3/week2/Visual promise/main.js
+++ javascript/javascript3/week2/Visual promise/main.js
@@ -1,30 +1,29 @@
-//One by one
-const red = document.querySelector("ul.marks li:nth-child(1)");
-const blue = document.querySelector("ul.marks li:nth-child(2)");
-const green = document.querySelector("ul.marks li:nth-child(3)");
-
-function translateOneByOne() {
-  moveElement(red, { x: 20, y: 300 })
-    .then(() => {
-      console.log("Red Element has been moved");
-      moveElement(blue, { x: 400, y: 300 }).then(() => {
-        console.log("Blue Element has beenmoved");
-        moveElement(green, { x: 400, y: 20 }).then(() => {
-          console.log("Green Element has been moved");
-        });
-      });
-    })
-
-    .catch((err) => console.log(err));
-}
-
-//All at once
 function translateAllAtOnce() {
   Promise.all([
-    moveElement(red, { x: 20, y: 300 }),
-    moveElement(blue, { x: 400, y: 300 }),
-    moveElement(green, { x: 400, y: 20 }),
+    moveElement(document.getElementById("red"), { x: 20, y: 300 }),
+    moveElement(document.getElementById("blue"), { x: 400, y: 300 }),
+    moveElement(document.getElementById("green"), { x: 400, y: 20 }),
   ]).then(() => {
     console.log("All moved");
   });
 }
+function translateOneByOne() {
+  moveElement(document.getElementById("red"), { x: 20, y: 300 })
+    .then(() => {
+      console.log("Red moved");
+      moveElement(document.getElementById("blue"), { x: 400, y: 300 }).then(
+        () => {
+          console.log("Blue moved");
+          moveElement(document.getElementById("green"), { x: 400, y: 20 }).then(
+            () => {
+              console.log("Green moved");
+            }
+          );
+        }
+      );
+    })
+    .catch((err) => console.log(err));
+}
+//translateOneByOne()
+
+translateAllAtOnce();
