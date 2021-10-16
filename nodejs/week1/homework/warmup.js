a354a2ac457d6c485ca7f1cf0be2cec9c1defa5e
suvarna <suvarna.akkala@gmail.com>
2021-10-16 18:27:50 +0530

    nodejs-week1

--- nodejs/week1/homework/warmup.js
+++ nodejs/week1/homework/warmup.js
@@ -1,14 +1,19 @@
 class Circle {
   constructor(radius) {
-    this.radius = radius;
+      this.radius = radius;
   }
   getDiameter() {
-    return 2 * this.radius;
+      return 2 * this.radius;
   }
   getCircumference() {
-    return 2 * 3.14 * this.radius;
+      return 2 * 3.14 * this.radius;
   }
   getArea() {
-    return this.radius * this.radius;
+      return 3.14 * this.radius * this.radius;
   }
-}
+};
+
+const circle = new Circle(10);
+console.log(circle.getDiameter());
+console.log(circle.getCircumference());
+console.log(circle.getArea());
