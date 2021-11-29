d0d18c70f9f8ccec74f6890773faff3eb0646888
suvarna <suvarna.akkala@gmail.com>
2021-11-29 08:24:01 +0530

    react--week2

--- react/week2/todo-app/src/Timer.js
+++ react/week2/todo-app/src/Timer.js
@@ -1,17 +1,17 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
 const Timer = () => {
-const [timeValue, setTimeValue] = useState(0);
+  const [timeValue, setTimeValue] = useState(0);
 
-setInterval(() => {
-    setTimeValue(timeValue+1)
-}, 1000);
+  setTimeout(() => {
+    setTimeValue(timeValue + 1);
+  }, 1000);
 
-    return (
-        <div>
-            <h4>You have used {timeValue} seconds on this website</h4>
-        </div>
-    );
-}
+  return (
+    <div>
+      <h4>You have used {timeValue} seconds on this website</h4>
+    </div>
+  );
+};
 
 export default Timer;
